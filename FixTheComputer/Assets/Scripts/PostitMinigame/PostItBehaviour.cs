848098648db047a08b3e9e84ef340865840fff50
EnergySlam <anijsbroodje@gmail.com>
2020-02-01 15:50:31 +0100

    Added window screens to post it and fishing

---
+++ FixTheComputer/Assets/Scripts/PostitMinigame/PostItBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostItBehaviour : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(HidePostit());
+    }
+
+    IEnumerator HidePostit()
+    {
+        yield return new WaitForSeconds(3f);
+        this.gameObject.SetActive(false);
+    }
+}
