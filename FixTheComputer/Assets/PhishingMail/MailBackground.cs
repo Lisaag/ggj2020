3497d3a8944b0348ef5c1fa0663e62759b788268
Lisa <lisatgroen@hotmail.com>
2020-02-01 13:16:23 +0100

    Phishing mail

---
+++ FixTheComputer/Assets/PhishingMail/MailBackground.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailBackground : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 backgroundSize = new Vector2(10, 10);
+
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.drawMode = SpriteDrawMode.Tiled;
+        spriteRenderer.size = backgroundSize;
+    }
+}
---
+++ FixTheComputer/Assets/PhishingMail/MailTrashbin.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailTrashbin : MonoBehaviour
+{
+    [SerializeField]
+    Color defaultColor;
+
+    [SerializeField]
+    Color[] rainbowColors;
+
+    [SerializeField]
+    float highlightSpeed;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = defaultColor;
+       // StartCoroutine(Rainbow(0));
+    }
+
+    void OnMouseOver()
+    {
+        spriteRenderer.color = Color.gray;
+    }
+
+    void OnMouseExit()
+    {
+        spriteRenderer.color = defaultColor;
+    }
+
+    public IEnumerator Rainbow(int index)
+    {
+        if (index == rainbowColors.Length) index = 0;
+        yield return new WaitForSeconds(highlightSpeed);
+        spriteRenderer.color = rainbowColors[index];
+        index++;
+        StartCoroutine(Rainbow(index));
+    }
+}
